9fd48f5a04e5ee62585724b8ce17d9eb1761d350
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-06-30 20:05:06 -0600

    Pass shelter index page spec

--- spec/features/shelters/shelters_spec.rb
+++ spec/features/shelters/shelters_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe "shelter index page", type: :feature do
+  it "can see the names of each shelter in the system" do
+    shelter_1 = Shelter.create(title:       "Good Boys Are Us")
+
+    shelter_2 = Shelter.create(title:       "I Don't Bite Hard")
+
+    visit '/shelters'
+
+    expect(page).to have_content(shelter_1.title)
+    expect(page).to have_content(shelter_2.title)
+  end
+end
