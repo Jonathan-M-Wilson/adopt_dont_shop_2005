4b9b4882914796168863b9a945e0adf407da8da6
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-02 12:11:27 -0600

    Add validations for model and spec

--- spec/features/shelters/shelters_spec.rb
+++ spec/features/shelters/shelters_spec.rb
@@ -7,9 +7,18 @@ require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
   it "can see the names of each shelter in the system" do
-    shelter_1 = Shelter.create(name:       "Good Boys Are Us")
+    shelter_1 = Shelter.create(name:       "Good Boys Are Us",
+                               address:     "1234 ABC Street",
+                               city:        "Denver",
+                               state:       "Colorado",
+                               zip:         80202)
 
-    shelter_2 = Shelter.create(name:       "I Don't Bite Hard")
+
+    shelter_2 = Shelter.create(name:       "I Don't Bite Hard",
+                               address:     "4321 CBA Street",
+                               city:        "Revned",
+                               state:       "Colorado",
+                               zip:         80202)
 
     visit '/shelters'
 
