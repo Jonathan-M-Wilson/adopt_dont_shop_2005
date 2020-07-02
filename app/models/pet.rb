77c9f37082f67790e21a1af82963c3b87735c0ad
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-02 13:40:58 -0600

    Add validations for pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,7 @@
 class Pet < ApplicationRecord
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :approximate_age
+  validates_presence_of :sex
+  validates_presence_of :sheltered_at
 end
