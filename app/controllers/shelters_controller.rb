b8666664f6d145ee76e97e7c51a7140b0041981d
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-02 11:56:54 -0600

    Create and update shelter passing

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,7 +3,31 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def new
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    Shelter.create(shelter_params)
+    redirect_to '/shelters'
+  end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update(shelter_params)
+    redirect_to "/shelters"
+  end
+
+  private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 end
