e25cf4e96b0fdfa797bb9058d4d31480f0da6968
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-02 13:35:03 -0600

    Add pet index and pass pet index spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200630224843) do
+ActiveRecord::Schema.define(version: 20200702191746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.float "approximate_age"
+    t.string "sex"
+    t.string "sheltered_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
