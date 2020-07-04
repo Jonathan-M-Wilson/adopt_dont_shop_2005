9a11fef9323698d8beb8c560bce0515c21190096
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-04 14:45:12 -0600

    Pass user story 12, delete pet

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
   patch '/pets/:id', to: 'pets#update'
+  delete '/pets/:id', to: 'pets#destroy' 
 
 end
